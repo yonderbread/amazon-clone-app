840b498420b784fb45c4cb1c66cb42eee263db36
yonderbread <33736622+yonderbread@users.noreply.github.com>
2023-11-23 00:28:52 -0700

    Checkout page styling

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -4,7 +4,7 @@ import CurrencyFormat from 'react-currency-format';
 function Subtotal() {
     return (
         <div className="subtotal">
-
+            <button>Proceed to Checkout</button>
         </div>
     )
 }
