8701b053a3106c4b47904ecd067db37e2d98d9d4
yonderbread <33736622+yonderbread@users.noreply.github.com>
2023-11-22 21:52:01 -0700

    Initial commit

---
+++ src/Header.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import SearchIcon from '@material-ui/icons/Search'
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'
+import { Link } from 'react-router-dom'
+import './Header.css'
+
+function Header() {
+    return (
+        <nav className="header">
+            {/* TODO: Make icon file local */}
+            <img className="header-logo" src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="logo"/>
+            <div className="header-search">
+                <input className="header-searchInput" type="text"/>
+                <SearchIcon className="header-searchIcon"/>
+            </div>
+            <div className="header-nav">
+                {/* First link */}
+                <Link className="header-link" to="/">
+                    <div className="header-option">
+                        <span className="header-option-lineOne">Hello, User</span>
+                        <span className="header-option-lineTwo">Sign In</span>
+                    </div>
+                </Link>
+                {/* Second link */}
+                <Link className="header-link" to="/">
+                    <div className="header-option">
+                        <span className="header-option-lineOne">Returns</span>
+                        <span className="header-option-lineTwo">& Orders</span>
+                    </div>
+                </Link>
+                {/* Third link */}
+                <Link className="header-link" to="/">
+                    <div className="header-option">
+                        <span className="header-option-lineOne">Hello, User</span>
+                        <span className="header-option-lineTwo">Sign In</span>
+                    </div>
+                </Link>
+            </div>
+            {/* Shopping basket icon */}
+            <div>
+                <Link className="header-link" to="/checkout">
+                    <div class="header-optionBasket">
+                        <ShoppingBasketIcon/>
+                        {/* Updating number of items in shopping basket */}
+                        <span className="header-option-lineTwo-basketCount">
+                            2
+                        </span>
+                    </div>
+                </Link>
+            </div>
+        </nav>
+    )
+}
+
+export default Header;
---
+++ src/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Login() {
+    return (
+        <div className="login">
+            <div className="login-container">
+                <h1>Sign In</h1>
+                <form>
+                    <h5>Email</h5>
+                    <input className="" type="email" value=""></input>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
