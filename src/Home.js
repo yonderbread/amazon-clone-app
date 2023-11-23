66d49c3df6861b356e57c2d4f4f5e657d0ec62e7
yonderbread <33736622+yonderbread@users.noreply.github.com>
2023-11-23 00:06:48 -0700

    Fix product styling

--- src/Home.js
+++ src/Home.js
@@ -20,12 +20,28 @@ function Home() {
                     image="https://m.media-amazon.com/images/I/91CUjdsS3ZL._AC_UL320_.jpg"
                 />
                 <Product
-                    id="000000001"
-                    title="Marhsall Origin 240-Watt 4x12 Inches Slant Extension Cabinet Amplifier PArt (ORI412A)"
-                    price={549.0}
+                    id="000000002"
+                    title="Boss RE-202 Space Echo Digital Delay Pedal "
+                    price={314.00}
                     rating={4}
-                    image="https://m.media-amazon.com/images/I/91CUjdsS3ZL._AC_UL320_.jpg"
+                    image="https://m.media-amazon.com/images/I/711xmHaToDL.__AC_SY300_SX300_QL70_FMwebp_.jpg"
                 />
+                <Product
+                    id="000000003"
+                    title="Pro Co RAT2 Distortion Pedal"
+                    price={74.0}
+                    rating={4}
+                    image="https://m.media-amazon.com/images/I/61gHNmcYvFL._AC_SX425_.jpg"
+                />
+                <Product
+                    id="000000004"
+                    title="Squier Affinity Series Deluxe Telecaster Electric Guitar, with 2-Year Warranty, Black, Maple Fingerboard"
+                    price={279.99}
+                    rating={4}
+                    image="https://m.media-amazon.com/images/I/41fGgZhH9fL._AC_SL1000_.jpg"
+                />
+            </div>
+            <div className="home-row">
                 <Product
                     id="000000001"
                     title="Marhsall Origin 240-Watt 4x12 Inches Slant Extension Cabinet Amplifier PArt (ORI412A)"
@@ -34,11 +50,25 @@ function Home() {
                     image="https://m.media-amazon.com/images/I/91CUjdsS3ZL._AC_UL320_.jpg"
                 />
                 <Product
-                    id="000000001"
-                    title="Marhsall Origin 240-Watt 4x12 Inches Slant Extension Cabinet Amplifier PArt (ORI412A)"
-                    price={549.0}
+                    id="000000002"
+                    title="Boss RE-202 Space Echo Digital Delay Pedal "
+                    price={314.00}
                     rating={4}
-                    image="https://m.media-amazon.com/images/I/91CUjdsS3ZL._AC_UL320_.jpg"
+                    image="https://m.media-amazon.com/images/I/711xmHaToDL.__AC_SY300_SX300_QL70_FMwebp_.jpg"
+                />
+                <Product
+                    id="000000003"
+                    title="Pro Co RAT2 Distortion Pedal"
+                    price={74.0}
+                    rating={4}
+                    image="https://m.media-amazon.com/images/I/61gHNmcYvFL._AC_SX425_.jpg"
+                />
+                <Product
+                    id="000000004"
+                    title="Squier Affinity Series Deluxe Telecaster Electric Guitar, with 2-Year Warranty, Black, Maple Fingerboard"
+                    price={279.99}
+                    rating={4}
+                    image="https://m.media-amazon.com/images/I/41fGgZhH9fL._AC_SL1000_.jpg"
                 />
             </div>
         </div>
