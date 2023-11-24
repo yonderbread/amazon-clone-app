234b18c0f1a0116b8a8f7261d44dd2fe35d6ae40
yonderbread <33736622+yonderbread@users.noreply.github.com>
2023-11-24 13:47:29 -0700

    trying to debug shopping basket

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,27 @@
 import React from "react"
 import './Product.css'
+import { useStateValue } from "./StateProvider";
 
 function Product({ id, title, image, price, rating }) {
+    const [{basket}, dispatch] = useStateValue();
+
+    console.log("Basket contents: ", basket)
+
+    const addToBasket = () => {
+
+        // Dispatch an action to the reducer
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating
+            }
+        });
+    };
+
     return (
         <div className="product">
             <div className="product-info">
@@ -19,7 +39,7 @@ function Product({ id, title, image, price, rating }) {
                 </div>
             </div>
             <img src={image} alt=""></img>
-            <button>Add to basket</button>
+            <button onClick={addToBasket}>Add to basket</button>
         </div>
     )
 }
