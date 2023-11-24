3d89aaede6c82cf312375d70de2dc6124c00baca
yonderbread <33736622+yonderbread@users.noreply.github.com>
2023-11-24 16:18:57 -0700

    Added footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./Header"
 import Checkout from './Checkout'
 import Login from "./Login"
 import Home from "./Home"
+import Footer from './Footer'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
                 <Routes>
                     <Route path="/checkout" element={[<Header />, <Checkout />]} />
                     <Route path="/login" element={<Login />} />
-                    <Route path="/" element={[<Header />, <Home />]} />
+                    <Route path="/" element={[<Header />, <Home />, <Footer />]} />
                 </Routes>
             </div>
         </Router>
---
+++ src/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className="footer">
+            <div className="footer-top">
+                <p>Back to Top</p>
+            </div>
+            <div className="footer-links">
+                <div className="footer-linkArea">
+                    <span>test</span>
+                </div>
+                <div className="footer-linkArea">
+                    <span>test</span>
+                </div>
+                <div className="footer-linkArea">
+                    <span>test</span>
+                </div>
+                <div className="footer-linkArea">
+                    <span>test</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
